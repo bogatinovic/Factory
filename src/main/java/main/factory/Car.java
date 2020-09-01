e797b3a01b1ee9bdf3c5fa5a474256b8d1d62c3a
Milos <milos.bogatinovic@novateq.rs>
2020-09-01 16:22:11 +0200

    Adds new constructor for class Car

--- src/main/java/main/factory/Car.java
+++ src/main/java/main/factory/Car.java
@@ -4,10 +4,41 @@ public class Car {
     
     private String model;
     private int buildYear;
-    private String color = "undefined";
+    private String color;
     private int maxSpeed;
     private int ccm;
     private int consumption;
+    private int milage;
+    
+    public Car(){
+        this.model = "undefined";
+        this.buildYear = -1;
+        this.color = "undefined";
+        this.maxSpeed = -1;
+        this.ccm = -1;
+        this.consumption = 0;
+        this.milage = 0;
+    }
+    
+    public Car(String customModel, int customBuildYear, String customColor, int customMaxSpeed, int customCcm){
+        this.model = customModel;
+        this.buildYear = customBuildYear;
+        this.color = customColor;
+        this.maxSpeed = customMaxSpeed;
+        this.ccm = customCcm;
+    }
+
+    public Car(String model, int buildYear, String color, int maxSpeed, int ccm, int consumption, int milage) {
+        this.model = model;
+        this.buildYear = buildYear;
+        this.color = color;
+        this.maxSpeed = maxSpeed;
+        this.ccm = ccm;
+        this.consumption = consumption;
+        this.milage = milage;
+    }
+    
+    
     
     public String getModel() {
         return this.model;
@@ -29,24 +60,24 @@ public class Car {
         return color;
     }
 
-    public void setColor(String color) {
-        this.color = color;
+    public void setColor(String customColor) {
+        this.color = customColor;
     }
 
     public int getMaxSpeed() {
         return maxSpeed;
     }
 
-    public void setMaxSpeed(int maxSpeed) {
-        this.maxSpeed = maxSpeed;
+    public void setMaxSpeed(int customMaxSpeed) {
+        this.maxSpeed = customMaxSpeed;
     }
 
     public int getCcm() {
         return ccm;
     }
 
-    public void setCcm(int ccm) {
-        this.ccm = ccm;
+    public void setCcm(int customCcm) {
+        this.ccm = customCcm;
     }
 
     public int getConsumption() {
@@ -56,6 +87,16 @@ public class Car {
     public void setConsumption(int consumption) {
         this.consumption = consumption;
     }
+
+    public int getMilage() {
+        return milage;
+    }
+
+    public void setMilage(int milage) {
+        this.milage = milage;
+    }
+    
+    
     
     public void printAttributes() {
         System.out.println("Model: " + this.getModel());
@@ -63,6 +104,12 @@ public class Car {
         System.out.println("Ccm: " + this.getCcm());
         System.out.println("Boja: " + this.getColor());
         System.out.println("Potrosnja: " + this.getConsumption());
+        System.out.println("Kilometraza: " + this.getMilage());
+        System.out.println("Maksimalna brzina: " + this.getMaxSpeed());
         System.out.println();
+    }
+    
+    public void goToTravel (int distance) {
+        this.setMilage(this.getMilage()+ distance);
     }
 }
--- src/main/java/main/factory/main.java
+++ src/main/java/main/factory/main.java
@@ -6,6 +6,7 @@ public class main {
         System.out.println("Pocetak");
         
         Car audi8 = new Car();
+        audi8.printAttributes();
         
         audi8.setModel("Audi A8");
         audi8.setBuildYear(2003);
@@ -22,11 +23,15 @@ public class main {
         System.out.println("Model automobila je " + fiatPunto.getModel());
         fiatPunto.printAttributes();
         
-        Car lada = new Car();
-        lada.setModel("Lada");
-                
-        System.out.println("Model automobila je " + lada.getModel());
+        Car lada = new Car("Lada Niva", 1990, "green", 120, 50);
         lada.printAttributes();
+//      System.out.println("Model automobila je " + lada.getModel());
+        
+        Car opel = new Car("Opel", 2002, "blue", 200, 1300, 7, 150000);
+        opel.printAttributes();
+        opel.goToTravel(200);
+        opel.printAttributes();
+        
     }
     
 }
